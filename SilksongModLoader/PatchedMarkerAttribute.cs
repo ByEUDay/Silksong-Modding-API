200372a029a0f9c734839a8e7bc70741a92ede40
ByEUDay <elurday@outlook.com>
2026-07-17 09:26:10 +0800

    Update PatchedMarkerAttribute.cs

--- SilksongModLoader/PatchedMarkerAttribute.cs
+++ SilksongModLoader/PatchedMarkerAttribute.cs
@@ -2,10 +2,6 @@ using System;
 
 namespace SilksongModLoader
 {
-    /// <summary>
-    /// Prepatcher 打桩成功后,会往 Assembly-CSharp 程序集上附加这个 Attribute,
-    /// 用来判断"是否已经打过桩",避免重复插入调用。
-    /// </summary>
     [AttributeUsage(AttributeTargets.Assembly)]
     public sealed class PatchedMarkerAttribute : Attribute
     {
